66cd47f515b3eb7d17a6697d9edf7dc9a0b970da
Obiwac <obiwac@gmail.com>
2018-11-21 20:07:52 +0100

    Debugging

--- glue.c
+++ glue.c
@@ -7,68 +7,63 @@ void mfree(void* ptr, unsigned long long bytes) { // for some reason, this was n
 	
 }
 
-signed long long load_rom(unsigned long long sdf) {
-	return 0;
-	
-}
-
 static kos_t kos;
 
-//~ #define ROM_PATH "ROM.zed"
-//~ static unsigned long long kos_roms_loaded = 0;
+#define ROM_PATH "ROM.zed"
+static unsigned long long kos_roms_loaded = 0;
 
-//~ signed long long load_rom(unsigned long long __path) {
-	//~ const char* ____path;
+signed long long load_rom(unsigned long long __path) {
+	const char* ____path;
 	
-	//~ if (kos_roms_loaded++) {
-		//~ GET_PATH((char*) __path);
-		//~ ____path = path;
+	if (kos_roms_loaded++) {
+		GET_PATH((char*) __path);
+		____path = path;
 		
-	//~ } else {
-		//~ ____path = (const char*) __path;
+	} else {
+		____path = (const char*) __path;
 		
-	//~ }
+	}
 	
-	//~ const char* path = ____path;
+	const char* path = ____path;
 	
 	//~ void*                 __pointer_current_program_previous = __pointer_current_program;
 	//~ __pointer__program_t* __pointer___this_previous          = __pointer___this;
 	
 	//~ program_t de_program;
-	//~ FILE* fp = fopen(path, "rb");
+	FILE* fp = fopen(path, "rb");
 	
-	//~ if (!fp) {
-		//~ printf("WARNING Could not open ROM file (%s)\n", path);
-		//~ kos_quit(&kos);
-		//~ exit(1);
+	if (!fp) {
+		printf("WARNING Could not open ROM file (%s)\n", path);
+		kos_quit(&kos);
+		exit(1);
 		
-	//~ }
+	}
 	
-	//~ fseek(fp, 0, SEEK_END);
-	//~ unsigned long long bytes = ftell(fp);
-	//~ rewind(fp);
+	fseek(fp, 0, SEEK_END);
+	unsigned long long bytes = ftell(fp);
+	rewind(fp);
 	
-	//~ char* rom = (char*) malloc(bytes);
-	//~ fread(rom, sizeof(char), bytes, fp);
+	char* rom = (char*) malloc(bytes);
+	fread(rom, sizeof(char), bytes, fp);
 	
 	//~ de_program.pointer = rom;
 	
-	//~ printf("Starting run setup phase ...\n");
+	printf("Starting run setup phase ...\n");
 	//~ program_run_setup_phase(&de_program);
 	
 	//~ while (!program_run_loop_phase(&de_program));
 	//~ program_free(&de_program);
 	
-	//~ #if !OLD_ROM_FORMAT
-		//~ mfree(rom, bytes);
-	//~ #endif
+	#if !OLD_ROM_FORMAT
+		mfree(rom, bytes);
+	#endif
 	
 	//~ __pointer_current_program = __pointer_current_program_previous;
 	//~ __pointer___this          = __pointer___this_previous;
 	
-	//~ return de_program.error_code;
+	return 0;//de_program.error_code;
 	
-//~ }
+}
 
 int main(void) {
 	printf("\nControl passed to the CW\n");
