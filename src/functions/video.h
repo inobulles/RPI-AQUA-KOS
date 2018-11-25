d5b278d41d586ada3e2c89150c9834702cd5487f
Obiwac <obiwac@gmail.com>
2018-11-25 18:38:55 +0100

    Just for once

--- src/functions/video.h
+++ src/functions/video.h
@@ -8,8 +8,6 @@ unsigned long long video_width (void) { return (unsigned long long) screen_width
 unsigned long long video_height(void) { return (unsigned long long) screen_height; }
 
 void video_flip(void) {
-	printf("%f\n", current_kos->delta);
-	
 	current_kos->time_1 = clock();
 	current_kos->delta = -(current_kos->time_2 - current_kos->time_1) / 1000000.0f * 1000.0f;
 	
