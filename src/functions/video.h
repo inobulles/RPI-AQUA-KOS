0ac45689047fb7938f5c2c8a8509ccba953e376a
Obiwac <obiwac@gmail.com>
2018-11-25 18:07:21 +0100

    Testing]

--- src/functions/video.h
+++ src/functions/video.h
@@ -1,6 +1,5 @@
 
 unsigned long long video_fps(void) {
-	printf("%f\n", current_kos->delta);
 	return 60;//(unsigned long long) current_kos->delta;
 	
 }
@@ -9,11 +8,13 @@ unsigned long long video_width (void) { return (unsigned long long) screen_width
 unsigned long long video_height(void) { return (unsigned long long) screen_height; }
 
 void video_flip(void) {
-	//~ current_kos->time_1 = clock();
-	//~ current_kos->delta = -(current_kos->time_2 - current_kos->time_1) / 1000000.0f * 1000.0f;
+	printf("%f\n", current_kos->delta);
+	
+	current_kos->time_1 = clock();
+	current_kos->delta = -(current_kos->time_2 - current_kos->time_1) / 1000000.0f * 1000.0f;
 	
 	flip(&current_kos->state);
-	//~ current_kos->time_2 = clock();
+	current_kos->time_2 = clock();
 	
 	#if KOS_3D_VISUALIZATION
 		glRotatef(1.0f, 0.0f, 1.0f, 0.0f);
