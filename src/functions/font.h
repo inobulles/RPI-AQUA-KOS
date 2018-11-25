76a30f09444df311159473f3214ce27b97a1557d
Obiwac <obiwac@gmail.com>
2018-11-25 12:21:05 +0100

    Fixed font temp pathds

--- src/functions/font.h
+++ src/functions/font.h
@@ -127,18 +127,21 @@ static void kos_font_create_text(kos_font_t* this, char* text) {
 		this->text = (char*) malloc(strlen(text));
 		strcpy(this->text,                 text);
 		
-		#define TEMP_TEXT_RESULT_IMAGE_BMP  ".__temp_text_result.bmp"
-		#define TEMP_TEXT_RESULT_IMAGE "root/" TEMP_TEXT_RESULT_IMAGE_BMP
-		#define TEMP_TEXT_RESULT_TEXT  "root/.__temp_text_result.txt"
+		#define TEMP_TEXT_RESULT_IMAGE      ".__temp_text_result.bmp"
+		#define TEMP_TEXT_RESULT_IMAGE_ROOT "root/" TEMP_TEXT_RESULT_IMAGE
+		
+		#define TEMP_TEXT_RESULT_TEXT       ".__temp_text_result.txt"
+		#define TEMP_TEXT_RESULT_TEXT_ROOT  "root/" TEMP_TEXT_RESULT_TEXT
 		
 		fs_write((unsigned long long) TEMP_TEXT_RESULT_TEXT, (unsigned long long) this->text, strlen(this->text));
 		
 		#define                          ORIGINAL_COMMAND_BYTES 256
 		char*   command = (char*) malloc(ORIGINAL_COMMAND_BYTES + strlen(this->text));
-		sprintf(command, "convert -background transparent -fill white -font \"%s\" -pointsize %d label:@" TEMP_TEXT_RESULT_TEXT " " TEMP_TEXT_RESULT_IMAGE, "Ubuntu", (int) this->size);
+		sprintf(command, "convert -background transparent -fill white -font \"%s\" -pointsize %d label:@" TEMP_TEXT_RESULT_TEXT_ROOT " " TEMP_TEXT_RESULT_IMAGE_ROOT, "Sans", (int) this->size);
 		system (command);
-		bmp_load(&this->bmp, (unsigned long long) TEMP_TEXT_RESULT_IMAGE_BMP);
-		system ("rm " TEMP_TEXT_RESULT_IMAGE " " TEMP_TEXT_RESULT_TEXT);
+		
+		bmp_load(&this->bmp,     (unsigned long long) TEMP_TEXT_RESULT_IMAGE);
+		system ("rm " TEMP_TEXT_RESULT_IMAGE_ROOT " " TEMP_TEXT_RESULT_TEXT_ROOT);
 		
 	}
 	
